40e0eb187624f3e6c7d141a4a42db37f4f2bc1eb
Thiago Felipe <thiagoflr1@gmail.com>
2024-12-18 17:43:59 -0300

    Subtotal & Total methods

--- src/main/java/com/coursejpa/course/entities/OrderItem.java
+++ src/main/java/com/coursejpa/course/entities/OrderItem.java
@@ -67,10 +67,15 @@ public class OrderItem implements Serializable {
 		id.setProduct(product);
 	}
 	
+	public Double getSubTotal() {
+		return price * quantity;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
+	
 
 	@Override
 	public boolean equals(Object obj) {
