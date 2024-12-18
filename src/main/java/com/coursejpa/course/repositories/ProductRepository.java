ab9b7acb182a453b0653c4e1c779fd46670eb151
Thiago Felipe <thiagoflr1@gmail.com>
2024-12-18 14:11:36 -0300

    Product

---
+++ src/main/java/com/coursejpa/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.coursejpa.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.coursejpa.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+}
