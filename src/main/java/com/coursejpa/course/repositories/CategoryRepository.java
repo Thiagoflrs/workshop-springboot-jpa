711dc4a830363d2dbdc6b429f2c365ac3f77c214
Thiago Felipe <thiagoflr1@gmail.com>
2024-12-18 13:40:22 -0300

    Category

---
+++ src/main/java/com/coursejpa/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.coursejpa.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.coursejpa.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+	
+}
